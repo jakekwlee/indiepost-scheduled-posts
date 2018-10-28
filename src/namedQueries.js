45f22484ec04ccc7cbc95e9b87d5f190168a291d
Jake Lee <bwv1050@gmail.com>
2018-10-28 22:52:32 +0900

    Version 1.1.3: - Fix sql: update column names

--- src/namedQueries.js
+++ src/namedQueries.js
@@ -2,8 +2,8 @@ module.exports = {
   PUBLISH_SCHEDULED_POSTS:
     "update Posts set status = 'PUBLISH' , modifiedAt = now() where status = 'FUTURE' and publishedAt < now()",
   SELECT_SCHEDULED_POSTS:
-    "select id, title, status, splash, featured from Posts where status = 'FUTURE' and publishedAt < now()",
-  UNSET_SPLASH_POSTS: "update Posts set splash = false where splash is true and status = 'PUBLISH'",
+    "select id, title, status, isSplash, isFeatured from Posts where status = 'FUTURE' and publishedAt < now()",
+  UNSET_SPLASH_POSTS: "update Posts set isSplash = false where isSplash is true and status = 'PUBLISH'",
   UNSET_FEATURE_POSTS:
-    "update Posts set featured = false where featured is true and status = 'PUBLISH'",
+    "update Posts set isFeatured = false where isFeatured is true and status = 'PUBLISH'",
 };
